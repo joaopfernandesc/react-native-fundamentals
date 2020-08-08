30b4c9363eb1affea1ed1f4a2d07baa3f2dddb26
João <joaopfernandesc@gmail.com>
2020-08-07 23:59:32 -0300

    Finished GoMarketPlace

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const value = products.reduce((sum, item) => {
+      return sum + item.price * item.quantity;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(value);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const quantity = products.reduce((sum, item) => {
+      return sum + item.quantity;
+    }, 0);
 
-    return 0;
+    return quantity;
   }, [products]);
 
   return (
